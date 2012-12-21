7603768f20e1074f9c5aa02b4cc11c60a364a58b
Pablo Karlsson <pablo.karlsson@gmail.com>
2012-12-21 05:26:04 +0100

    Fixed some syntax and indention

--- server/skott.adb
+++ server/skott.adb
@@ -1,18 +1,12 @@
--------------------------
---Denna procedur flyttar kollar om bollen och spelaren skall flyttas
--------------------------
-with ada.integer_text_IO;
-use ada.integer_text_io;
-with ada.text_io;
-use ada.text_io;
-with move;
+--------------------------------------------------------------------------------
+-- This procedure allows for the movement of the ball.
+--------------------------------------------------------------------------------
+with ada.Integer_Text_IO; use ada.Integer_Text_IO;
+with ada.Text_IO; use ada.Text_IO;
+with Move;
 with Startpos;
-with Typer;
+with Typer; use Typer;
 with Goal;
-use Typer;
-
-
-
 
 procedure skott(Plan_Info: in out Planinfo_Type;
                 Riktning: in Integer;
@@ -20,33 +14,26 @@ procedure skott(Plan_Info: in out Planinfo_Type;
                 Handelse: out Natural;
                 Valid: out Boolean) is
 
-
    Kopia: Planinfo_Type:= Plan_Info;
 
 begin
 Valid:=True;
 
    Move(Plan_Info(0).X,Plan_Info(0).Y,Riktning);
-   -------
 
-      for I in Planinfo_Type'First+1..Antal_Spelare loop
-         if Plan_Info(0).X=Plan_Info(I).X and Plan_Info(0).Y=Plan_Info(I).Y then
-            Plan_Info:=Kopia;
-            Valid:=False;
-         end if;
-      end loop;
+    for I in Planinfo_Type'First+1..Antal_Spelare loop
+       if Plan_Info(0).X=Plan_Info(I).X and Plan_Info(0).Y=Plan_Info(I).Y then
+        Plan_Info:=Kopia;
+        Valid:=False;
+      end if;
+    end loop;
 
-
-
-
-
-   --------------------------------------------
    --nu kollas det om bollen är för nära "mål"kanten
    if Plan_Info(0).X=Spelplan_X'First+1 and Plan_Info(0).Y in Spelplan_Y'First+1..Spelplan_Y'Last-1 and  Plan_Info(0).Y Not in 13..18 then
       Plan_Info:=Kopia;
    elsif Plan_Info(0).Y=Spelplan_Y'First+1 or Plan_Info(0).Y=Spelplan_Y'Last-1 then
       Plan_Info:=Kopia;
-       Valid:=False;
+      Valid:=False;
    end if;
 
    if  Plan_Info(0).X=Spelplan_X'Last-1 and Plan_Info(0).Y in  Spelplan_Y'First+1..Spelplan_Y'Last-1 and Plan_Info(0).Y not in 13..18 then
@@ -54,8 +41,7 @@ Valid:=True;
        Valid:=False;
    elsif Plan_Info(0).Y=Spelplan_Y'First+1 or Plan_Info(0).Y=Spelplan_Y'Last-1 then
       Plan_Info:=Kopia;
-       Valid:=False;
-
+      Valid:=False;
    end if;
 
    --kollar om det är mål, om det är det flyttas bollen till mitten och alla spelare flyttas till startpositionerna...
@@ -67,26 +53,15 @@ Valid:=True;
    --detta förhindrar att bollen fastnar i stolpen...[fulkod]
    if Plan_Info(0).X= Spelplan_X'First or Plan_Info(0).X= Spelplan_X'Last then
       Plan_Info:=Kopia;
-       Valid:=False;
+      Valid:=False;
    end if;
-
-
-
-
 
    --kollar om spelaren är utanför planen
    for Count in  Planinfo_Type'First+1..Antal_Spelare loop
       if Plan_Info(Count).X=Spelplan_X'First or Plan_Info(Count).X=Spelplan_X'Last or Plan_Info(Count).Y=Spelplan_y'First or Plan_Info(Count).Y=Spelplan_Y'Last then
          Plan_Info:=Kopia;
-          Valid:=False;
+         Valid:=False;
       end if;
    end loop;
 
-
-
-
-
 end skott;
--------------
---fråga petter om du inte fattar, han orkar inte skriva beskrivning just nu
---------------
--- server/skottkoll.adb
+++ server/skottkoll.adb
@@ -1,9 +1,6 @@
-with ada.integer_text_IO;
-use ada.integer_text_io;
-with ada.text_io;
-use ada.text_io;
-with Typer;
-use Typer;
+with ada.integer_text_IO; use ada.Integer_Text_IO;
+with ada.Text_IO; use ada.Text_IO;
+with Typer; use Typer;
 
 procedure skottkoll(Plan_Info: in Planinfo_Type;
                     Spelarnummer: in Integer;
@@ -12,59 +9,55 @@ procedure skottkoll(Plan_Info: in Planinfo_Type;
 
    Xdiff,Ydiff: Integer;
 
-
 begin
 
-   Valid:=False;
-
- Xdiff:=Plan_Info(0).X-Plan_Info(Spelarnummer).X;
- Ydiff:=Plan_Info(0).Y-Plan_Info(Spelarnummer).Y;
-
+  Valid:=False;
+  Xdiff:=Plan_Info(0).X-Plan_Info(Spelarnummer).X;
+  Ydiff:=Plan_Info(0).Y-Plan_Info(Spelarnummer).Y;
 
   if Xdiff=1 then
      case Ydiff is
-        when -1 =>
-           Riktning:=9;
-           Valid:=True;
-        when 0 =>
-           Riktning:=6;
-             Valid:=True;
-        when 1 =>
-           Riktning:=3;
-             Valid:=True;
-        when others =>
+       when -1 =>
+         Riktning:=9;
+         Valid:=True;
+       when 0 =>
+         Riktning:=6;
+         Valid:=True;
+       when 1 =>
+         Riktning:=3;
+         Valid:=True;
+       when others =>
            null;
      end case;
-       end if;
+  end if;
 
-     if Xdiff=0 then
-     case Ydiff is
-        when -1 =>
-           Riktning:=8;
-           Valid:=True;
-        when 1 =>
-           Riktning:=2;
-             Valid:=True;
-        when others =>
-           null;
-     end case;
-     end if;
+  if Xdiff=0 then
+    case Ydiff is
+      when -1 =>
+        Riktning:=8;
+        Valid:=True;
+      when 1 =>
+        Riktning:=2;
+        Valid:=True;
+      when others =>
+        null;
+    end case;
+  end if;
 
-     if Xdiff=-1 then
-     case Ydiff is
-        when -1 =>
-           Riktning:=7;
-           Valid:=True;
-        when 0 =>
-           Riktning:=4;
-             Valid:=True;
-        when 1 =>
-           Riktning:=1;
-             Valid:=True;
-        when others =>
-           null;
-     end case;
-     end if;
+  if Xdiff=-1 then
+    case Ydiff is
+      when -1 =>
+        Riktning:=7;
+        Valid:=True;
+      when 0 =>
+        Riktning:=4;
+        Valid:=True;
+      when 1 =>
+        Riktning:=1;
+        Valid:=True;
+      when others =>
+        null;
+    end case;
+  end if;
 
-
-     end Skottkoll;
+end Skottkoll;
